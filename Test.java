e541df650130d1fa360da666295c31053ff174cc
shashi <shashikumar3089@gmail.com>
2024-03-01 21:54:26 +0530

    added test.java

---
+++ Test.java
@@ -0,0 +1,19 @@
+package examples;
+
+import java.util.HashMap;
+
+public class TEst {
+
+	public static void main(String[] args) {
+		
+		public void dev1() {
+			
+		}
+		
+		public void dev2() {
+			
+			
+
+	}
+
+}
