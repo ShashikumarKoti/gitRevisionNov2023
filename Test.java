87182820ac3ae0706d4b1a34d5d3b506ca118729
shashi <shashikumar3089@gmail.com>
2024-03-01 22:13:30 +0530

    cleared test.java

--- Test.java
+++ Test.java
@@ -7,15 +7,12 @@ public class TEst {
 	public static void main(String[] args) {
 		
 		public void dev1() {
-		line1 added by dev1
-		line2 added by dev1
-		line2 added by master
+		
 
 		}
 		
 		public void dev2() {
-		line1 added by dev2
-		line2 added by dev2
+		
 		}
 			
 
